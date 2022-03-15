f31ba0ed8cfd75f793f7746839485397b73be177
MarginCode <jerrywang015@gmail.com>
2022-03-15 23:14:39 +0800

    Update excel.py

--- excel.py
+++ excel.py
@@ -1,5 +1,17 @@
+from ast import Return
+from typing import List
 import xlwings as xw
-#将二维数组写入Excel
+
+
+#替换三维列表中的空list项,将三维列表转换为二维（输入三维列表中的第三维直接删除）
+def noneListToNA(data3darray):
+    for i in range(len(data3darray)):
+        for j in range(len(data3darray[i])):
+            if type(data3darray[i][j])==List: data3darray[i][j]=="NA"
+    return data3darray
+
+
+#将二维列表写入Excel
 def outPutData(xlsx_path,sheet_name,begin_cell,data_2d_array):
     app=xw.app(visible=False, add_book=False)
     try:
